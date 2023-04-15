062d6cc87f4a4adf1e01ce31bcdc6d44e5ef2dd6
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-16 01:35:27 +0300

    Created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center bg-purple-500 h-12 p-3 justify-between text-neutral-100">
+      <div className="font-bold">Khazziro Chat</div>
+      <div className="flex gap-3">
+        <img
+          src="https://images.pexels.com/photos/16161517/pexels-photo-16161517.jpeg"
+          alt=""
+          className="bg-neutral-100 h-6 w-6 rounded-full object-cover "
+        />
+        <span>Renato</span>
+        <button className="bg-purple-400 text-neutral-100 text-xs px-2">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
