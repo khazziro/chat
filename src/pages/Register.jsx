6e4dde3b20ad3fed20156e64d3bfb6e67b8704be
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-20 01:03:37 +0300

    Added user registration functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,29 @@
 import classNames from "classnames";
 import { RiImageAddFill } from "react-icons/all.js";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { auth } from "../firebase.js";
+import { useState } from "react";
 
 const inputStyle = classNames(
   "py-4 px-7 border-b border-b-indigo-300 placeholder:text-neutral-300"
 );
+
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].value;
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center bg-indigo-200 h-screen">
       <div className="flex flex-col items-center bg-white px-16 py-5 rounded-xl gap-3">
@@ -12,7 +31,7 @@ const Register = () => {
           Khazziro Chat
         </div>
         <div className="title font-bold text-indigo-400">Register</div>
-        <form className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             className={inputStyle}
             type="text"
@@ -35,6 +54,7 @@ const Register = () => {
           <button className="bg-indigo-300 p-3 font-bold cursor-pointer rounded-md text-white">
             Sign Up
           </button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p className="text-indigo-400 text-sm mt-3 cursor-pointer">
           You do have an account? Login!
