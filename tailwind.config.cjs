434883bacdcca24d02451a062f0b0e0aafcf79ea
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-19 17:35:08 +0300

    Added screen sizes for responsive design to tailwind css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      mobile: {max: "480px"},
+      tablet: {max: "768px"},
+      laptop: {max: "1200px"}
+    },
     extend: {},
   },
   plugins: [],
