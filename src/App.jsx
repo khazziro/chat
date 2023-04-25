fd37c03eeccc315f40934b3d04dcfc596dc42f3d
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-25 13:41:19 +0300

    Add routing and authentication logic to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext.jsx";
+
 function App() {
-  return <div>Hello world!</div>;
+  const { currentUser } = useContext(AuthContext);
+
+  // Log the current user for debugging purposes
+  // console.log(currentUser);
+
+  return (
+    // Use BrowserRouter for routing
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          {/* Set Home as the default route */}
+          <Route index element={<Home />} />
+
+          {/* Allow unauthenticated users to access login and register pages */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
