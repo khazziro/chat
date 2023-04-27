c2aa977035a17fb1907f04e6dfe3ce9181ef8893
Renato-Niicolas <93095948+khaziro@users.noreply.github.com>
2023-04-27 12:47:23 +0300

    Added a Protected Route if the user isn't logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext.jsx";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
-
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to={"/login/"} />;
+    }
+  };
   // Log the current user for debugging purposes
   // console.log(currentUser);
 
@@ -17,8 +21,14 @@ function App() {
       <Routes>
         <Route path="/">
           {/* Set Home as the default route */}
-          <Route index element={<Home />} />
-
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           {/* Allow unauthenticated users to access login and register pages */}
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
